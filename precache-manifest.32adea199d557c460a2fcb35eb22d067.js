a6cebfb1be221871298b1ebc3c4e0536c8d78f49
Abhaya Jeyaraman <abhaya0405@gmail.com>
2019-11-25 11:41:40 +0530

    Updates

---
+++ precache-manifest.32adea199d557c460a2fcb35eb22d067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e3db8d90129950e6a08a7b5f5395a4",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "066d05aabfb796fae4e2",
+    "url": "/todo-list/static/css/main.65d2f035.chunk.css"
+  },
+  {
+    "revision": "fca06172a3363bec6a8d",
+    "url": "/todo-list/static/js/2.a2c21eb4.chunk.js"
+  },
+  {
+    "revision": "066d05aabfb796fae4e2",
+    "url": "/todo-list/static/js/main.3ac3fd31.chunk.js"
+  },
+  {
+    "revision": "f93898e5356ea7cd0a6f",
+    "url": "/todo-list/static/js/runtime-main.7417797d.js"
+  }
+]);
